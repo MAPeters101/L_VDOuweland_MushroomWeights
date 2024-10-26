7f107fe08a5563b16e042462615dc622fc4f879e
Mark Peters <mapeters101@earthlink.net>
2024-10-26 18:03:16 -0400

    Lesson25:  Work on lesson 5:30 to 8:10

--- main.py
+++ main.py
@@ -7,6 +7,24 @@ population_count = 20 # Combinations to choose the best solution from.
 rounds = 1  # Evolutions count
 chromosomes = [[random.choice([True, False]) for _ in mushroom_weights] for _ in range(population_count)]
 
+class Weighted:
+    max_weight = 1300  # grams
+    def __init__(self, mushroom_weights, chromosome):
+        self.mushrooms_in_bag = [w for w, c in zip(mushroom_weights, chromosome) if c is True]
+        self.total_weight = sum(self.mushrooms_in_bag)
+        if self.total_weight > self.max_weight:
+            self.total_weight = 0
+
+    def __lt__(self, other):
+        # Rule: less difference between max_weight and total_weight, results in higher ranking.
+        return self.max_weight - self.total_weight < self.max_weight - other.total_weight
+
 for r in range(rounds):
     print(f"=== ROUND {r}, population {len(chromosomes)} ===")
+    weighted = [Weighted(mushroom_weights, chromosome) for chromosome in chromosomes]
+    for w in sorted(weighted):
+        print(w.total_weight)
+
+
+
 
